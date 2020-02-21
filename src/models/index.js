9d07b2baa5d1f430fed476801ac8336909a3a116
imdar1 <13517002@std.stei.itb.ac.id>
2020-02-21 09:30:31 +0700

    Fix include path for section model

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,4 @@
 import userModel from './user';
-import sectionModel from './user';
+import sectionModel from './section';
 
 export const MongoModels = [{ name: 'userModel', model: userModel },{ name: 'sectionModel', model : sectionModel}];
