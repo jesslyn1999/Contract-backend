95e186f180cc966fbd63cd1001797c1fb8bda6d4
Anzaldi Sulaiman Oemar <13517098@std.stei.itb.ac.id>
2020-04-07 15:40:22 +0700

    Completed all method

--- src/services/tag.js
+++ src/services/tag.js
@@ -24,4 +24,51 @@ const setNewTag = (keyString, valueString) =>
             }
         });
     });
-}
+};
+
+const deleteTag = (keyString) =>
+{
+    return new Promise((resolve, reject) =>
+    {
+        const tagModelInstance = Container.get('tagModel');
+        
+        tagModelInstance.findOneAndRemove({key: keyString}, function(err)
+        {
+            if (err)
+            {
+                reject(err);
+            }
+            else
+            {
+                resolve({success: true});
+            }
+        });
+    });
+};
+
+const updateTag = (keyString, oldValue, newValue) =>
+{
+    return new Promise((resolve, reject) =>
+    {
+        const tagModelInstance = Container.get('tagModel');
+        
+        tagModelInstance.findOneAndRemove({key: keyString, value: oldValue}, {key: keyString, value: oldValue}, function(err)
+        {
+            if (err)
+            {
+                reject(err);
+            }
+            else
+            {
+                resolve({success: true});
+            }
+        });
+    });
+};
+
+export default
+{
+    setNewTag,
+    deleteTag,
+    updateTag
+};
