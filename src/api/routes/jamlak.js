4aa26c6502037aa73652e0736fd0f95c184fd8bf
Yoel Susanto <yoelsusantopro@yahoo.com>
2020-04-08 01:53:13 +0700

    Add jamlak API

---
+++ src/api/routes/jamlak.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { Container } from 'typedi';
+
+const route = Router();
+
+export default app => {
+    const jamlakService = Container.get('jamlakService');
+    const logger = Container.get('logger');
+
+    // Get Jamlak
+    route.get('/getjamlak/:nomor_jamlak', (req, res) => {
+        let nomor_jamlak = req.params.nomor_jamlak;
+        jamlakService
+            .getJamlak(nomor_jamlak)
+            .then(data_jamlak => {
+                return res.json({
+                    data: data_jamlak,
+                    request: { success: true, message: null },
+                });
+            })
+            .catch(err => {
+                logger.error(
+                    `[JamlakRoute][GetJamlak]: Failed to get jamlak. ${err}`,
+                );
+                return res.status(500).json({
+                    request: {
+                        success: false,
+                        message:
+                            'Internal server error, report to admin for assistance',
+                    },
+                });
+            });
+    });
+
+    app.use('/jamlak', route);
+};
