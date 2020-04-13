1aa93d10153bf5d2793fcaae5e86ad9f9e19c89f
Anzaldi Sulaiman Oemar <13517098@std.stei.itb.ac.id>
2020-04-13 20:58:02 +0700

    New regex algorithm

--- src/services/form.js
+++ src/services/form.js
@@ -2,41 +2,16 @@ const getAllTag = (id_template, listOfTag) =>
 {
     return new Promise((resolve, reject) =>
     {
-        var items, patt, str, result;
+        var items, pattern, str, result;
         listOfTag = [];
         // str = Ambil isi template dari id_template
 
-        patt = /Rp.{1,11}\.\d\d\d/i;
-        if (patt.test(str))
-        {
-            for (var i = 0; i < patt.length; ++i)
-            {
-                items.key = "nilai_uang";
-                items.value = str.match(patt)[i];
-                listOfTag.push(items);
-            }
-        }
+        pattern = /:(\w+)>>/g;
+        result = pattern.exec(str);
 
-        patt = /\d{1,2} .{0,9} 2\d\d\d/i;
-        if (patt.test(str))
+        while (result != null)
         {
-            for (var i = 0; i < patt.length; ++i)
-            {
-                items.key = "tanggal";
-                items.value = str.match(patt)[i];
-                listOfTag.push(items);
-            }
-        }
-
-        patt = /\d+\W.*[^\w\s]\d\d\d\d/i;
-        if (patt.test(str))
-        {
-            for (var i = 0; i < patt.length; ++i)
-            {
-                items.key = "nomor";
-                items.value = str.match(patt)[i];
-                listOfTag.push(items);
-            }
+            listOfTag.push(result[1]);
         }
 
         if (err)
