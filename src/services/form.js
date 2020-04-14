692cf1675fd78c33b8e9c2310954aa06f928f01a
Anzaldi Sulaiman Oemar <13517098@std.stei.itb.ac.id>
2020-04-14 15:35:51 +0700

    Update form.js

--- src/services/form.js
+++ src/services/form.js
@@ -1,31 +1,28 @@
-const getAllTag = (id_template, listOfTag) =>
-{
-    return new Promise((resolve, reject) =>
-    {
+import { Container } from 'typedi';
+
+const getAllTag = (id_template, listOfTag) => {
+    return new Promise((resolve, reject) => {
+        let templateService = Container.get('templateService');
+
         var items, pattern, str, result;
         listOfTag = [];
-        // str = Ambil isi template dari id_template
+        str = templateService.getTemplateById(id_template);
 
         pattern = /:(\w+)>>/g;
         result = pattern.exec(str);
 
-        while (result != null)
-        {
+        while (result != null) {
             listOfTag.push(result[1]);
         }
 
-        if (err)
-        {
+        if (err) {
             reject(err);
-        }
-        else
-        {
+        } else {
             resolve(listOfTag);
         }
     });
 };
 
-export default
-{
-    getAllTag
+export default {
+    getAllTag,
 };
