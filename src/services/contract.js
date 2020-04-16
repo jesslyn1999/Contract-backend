d7e3ec5ba243347640cd9a5b277794bb88531c64
Yoel Susanto <yoelsusantopro@yahoo.com>
2020-04-16 20:38:02 +0700

    Generate contract, get list sppbj and contract

---
+++ src/services/contract.js
@@ -0,0 +1,126 @@
+import { Container } from 'typedi';
+import { replaceAll, getDocxFromCloudmersive } from '../helper';
+import _ from 'lodash';
+
+const generateContract = (template_id, id_sppbj, id_jamlak, form_data) => {
+    // Services
+    const logger = Container.get('logger');
+    // Models
+    const templateModel = Container.get('templateModel');
+    const sppbjModel = Container.get('sppbjModel');
+    const jamlakModel = Container.get('jamlakModel');
+
+    return new Promise(async (resolve, reject) => {
+        let { content } = await templateModel
+            .findById(template_id)
+            .catch(reject);
+        let { data_pemenang, data_form, no_ssppbj } = await sppbjModel
+            .findById(id_sppbj)
+            .catch(reject);
+        let jamlak = await jamlakModel.findById(id_jamlak).catch(reject);
+
+        let getData = { ...data_pemenang, ...data_form, no_ssppbj, ...jamlak };
+        let capitalizedGetData = _.mapKeys(getData, (value, key) => {
+            return key.toUpperCase();
+        });
+
+        let populatedContent = replaceAll(
+            content,
+            capitalizedGetData,
+            form_data,
+        );
+
+        getDocxFromCloudmersive(populatedContent)
+            .then(async res => {
+                if (res.status === 200) {
+                    let saveObject = {
+                        no_contract: getData.no_contract,
+                        id_sppbj,
+                        id_jamlak,
+                        template_id,
+                        form_data,
+                        generate_document: {
+                            data: res.data,
+                        },
+                    };
+                    await saveContract(saveObject).catch(reject);
+                    return resolve({
+                        binary_data: res.data,
+                        name: `Contract-${getData.no_contract}.docx`,
+                    });
+                }
+                throw new Error(`Cloudmersive request doesn't end with 200 OK`);
+            })
+            .catch(err => {
+                console.log(err);
+                logger.error(
+                    `[ContractService][GenerateContract]: Failed to generate contract using Cloudmersive API. ${err}`,
+                );
+            });
+    });
+};
+
+const saveContract = saveObject => {
+    return new Promise(async (resolve, reject) => {
+        const contractModel = Container.get('contractModel');
+        const newContractInstance = new contractModel({ ...saveObject });
+
+        newContractInstance
+            .save()
+            .then(resolve)
+            .catch(reject);
+    });
+};
+
+const getAllContract = () => {
+    return new Promise((resolve, reject) => {
+        const contractModelInstance = Container.get('contractModel');
+        const logger = Container.get('logger');
+
+        contractModelInstance
+            .find({})
+            .select('-createdAt -updatedAt -__v -generated_document')
+            .then(result => {
+                resolve(result);
+            })
+            .catch(err => {
+                logger.error(
+                    `[ContractService][GetAllContract]: Failed to get all contract. ${err}`,
+                );
+                reject(err);
+            });
+    });
+};
+
+const getContractByPage = (page, perPage) => {
+    page = page || 1;
+    perPage = parseInt(perPage) || 9;
+    return new Promise(async (resolve, reject) => {
+        const contractModelInstance = Container.get('contractModel');
+        const logger = Container.get('logger');
+
+        const sectionsCount = await contractModelInstance.countDocuments({});
+        contractModelInstance
+            .find({})
+            .select('-createdAt -updatedAt -__v -generated_document')
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .then(result => {
+                const pages = Math.ceil(sectionsCount / perPage);
+                resolve([result, pages]);
+            })
+            .catch(err => {
+                logger.error(
+                    `[ContractService][GetContractByPage]: Failed to get contract by pages. ${err}`,
+                );
+                reject(err);
+            });
+    });
+};
+
+export default {
+    generateContract,
+    saveContract,
+    getAllContract,
+    getContractByPage,
+};
---
+++ src/services/sppbj.js
@@ -0,0 +1,129 @@
+import { Container } from 'typedi';
+import { replaceAll, getDocxFromCloudmersive } from '../helper';
+
+const generateSPPBJ = (id_template, data_pemenang, form_data) => {
+    return new Promise(async (resolve, reject) => {
+        const templateService = Container.get('templateService');
+        const logger = Container.get('logger');
+
+        let template = await templateService
+            .getTemplateById(id_template)
+            .catch(reject);
+
+        let templateContent = template.content;
+
+        let populatedContent = replaceAll(
+            templateContent,
+            data_pemenang,
+            form_data,
+        );
+        // code for generating pdf
+        // const browser = await puppeteer.launch();
+        // const page = await browser.newPage();
+        // let csspath = path.join(
+        //     __dirname,
+        //     '../resource/ckeditor-content-style.css',
+        // );
+        // await page.addStyleTag({
+        //     path: csspath,
+        // });
+        // await page.setContent(populatedContent);
+        // const pdf = await page.pdf({ format: 'A4' });
+        // await browser.close();
+
+        getDocxFromCloudmersive(populatedContent)
+            .then(async res => {
+                if (res.status === 200) {
+                    await saveSPPBJ(
+                        id_template,
+                        data_pemenang,
+                        form_data,
+                        res.data,
+                    ).catch(reject);
+                    return resolve({
+                        binary_data: res.data,
+                        name: 'SPPBJ.docx',
+                    });
+                }
+                throw new Error(`Cloudmersive request doesn't end with 200 OK`);
+            })
+            .catch(err => {
+                console.log(err);
+                logger.error(
+                    `[SPPBJService][GenerateSPPBJ]: Failed to generate SPPBJ using Cloudmersive API. ${err}`,
+                );
+            });
+    });
+};
+
+const saveSPPBJ = (template_id, data_pemenang, data_form, pdf_data) => {
+    return new Promise(async (resolve, reject) => {
+        const sppbjModel = Container.get('sppbjModel');
+        const newSppbjInstance = new sppbjModel({
+            template_id,
+            data_pemenang,
+            data_form,
+            generated_document: {
+                data: pdf_data,
+            },
+        });
+
+        newSppbjInstance
+            .save()
+            .then(resolve)
+            .catch(reject);
+    });
+};
+
+const getAllSppbj = () => {
+    return new Promise((resolve, reject) => {
+        const sppbjModelInstance = Container.get('sppbjModel');
+        const logger = Container.get('logger');
+
+        sppbjModelInstance
+            .find({})
+            .select('-createdAt -updatedAt -__v -_id')
+            .then(result => {
+                resolve(result);
+            })
+            .catch(err => {
+                logger.error(
+                    `[SPPBJService][GetAllSPPBJ]: Failed to get all sppbj. ${err}`,
+                );
+                reject(err);
+            });
+    });
+};
+
+const getSppbjByPage = (page, perPage) => {
+    page = page || 1;
+    perPage = parseInt(perPage) || 9;
+    return new Promise(async (resolve, reject) => {
+        const sppbjModelInstance = Container.get('sppbjModel');
+        const logger = Container.get('logger');
+
+        const sectionsCount = await sppbjModelInstance.countDocuments({});
+        sppbjModelInstance
+            .find({})
+            .select('-createdAt -updatedAt -__v -generated_document')
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .then(result => {
+                const pages = Math.ceil(sectionsCount / perPage);
+                resolve([result, pages]);
+            })
+            .catch(err => {
+                logger.error(
+                    `[SPPBJService][GetSPPBJByPage]: Failed to get sections by pages. ${err}`,
+                );
+                reject(err);
+            });
+    });
+};
+
+export default {
+    generateSPPBJ,
+    saveSPPBJ,
+    getAllSppbj,
+    getSppbjByPage,
+};
