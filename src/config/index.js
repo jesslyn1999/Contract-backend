976e064ece01708e063c247902f6ce0b39e27992
Yoel Susanto <yoelsusantopro@yahoo.com>
2020-05-18 14:41:47 +0700

    Update config loading

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 // if test, read env from CI predefined environment
+console.log('NODE_ENV', process.env.NODE_ENV);
 if (process.env.NODE_ENV !== 'test' || process.env.NODE_ENV !== 'heroku') {
     const configLoading = dotenv.config({ path: `${__dirname}/../../.env` });
 
